a2977774e8c4f181567b90f3372cf31cf1c8e86d
Denys <glushychden.10@gmail.com>
2026-01-17 19:49:26 +0200

    Changing code because last version have mistakes with getting currency without book map. Now code getting symbols with book map.

--- data.py
+++ data.py
@@ -2,19 +2,33 @@ import requests
 
 
 def get_binance_futures_usdt():
-    base_url = "https://fapi.binance.com"
+    base = "https://fapi.binance.com"
 
-    # 1. Всі тікери (bid/ask + volume)
+    # Bid / Ask - берем для порівняння ф'ючерсів
+    book = requests.get(
+        f"{base}/fapi/v1/ticker/bookTicker", timeout=5
+    ).json()
+
+    # 24h volume - об'єм монети за 24 години
     tickers = requests.get(
-        f"{base_url}/fapi/v1/ticker/24hr", timeout=5
+        f"{base}/fapi/v1/ticker/24hr", timeout=5
     ).json()
 
-    # 2. Funding
+    # Funding
     funding = requests.get(
-        f"{base_url}/fapi/v1/premiumIndex", timeout=5
+        f"{base}/fapi/v1/premiumIndex", timeout=5
     ).json()
 
-    # Приводимо funding в словник
+    # Книга ордерів
+    book_map = {
+        b["symbol"]: {
+            "bid": float(b["bidPrice"]),
+            "ask": float(b["askPrice"])
+        }
+        for b in book
+    }
+
+    # Книга фандінгу
     funding_map = {
         f["symbol"]: float(f["lastFundingRate"])
         for f in funding
@@ -22,18 +36,24 @@ def get_binance_futures_usdt():
 
     result = {}
 
+    # Цикл для перевірки ф'ючерса, щоб брати саме USDT і символ має мати книгу ордерів
     for t in tickers:
-        symbol = t["symbol"]
+        symbol = t.get("symbol")
 
-        # Берем тільки USDT-фьючерси
-        if not symbol.endswith("USDT"):
+        if not symbol or not symbol.endswith("USDT"):
             continue
 
+        if symbol not in book_map:
+            continue
+
+        # Систематизація інформації
         result[symbol] = {
-            "bid": float(t["bidPrice"]),
-            "ask": float(t["askPrice"]),
+            **book_map[symbol],
             "volume_usdt_24h": float(t["quoteVolume"]),
             "funding": funding_map.get(symbol)
         }
 
     return result
+
+
+print(get_binance_futures_usdt()['BTCUSDT'])
