85c8721782919cf4a7e97ef3030ea8b6dfcadfb0
Denys <glushychden.10@gmail.com>
2026-01-23 22:11:00 +0200

    Add function get_better_funding for getting better funding separately from function get_funding in older version get_better_funding.

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ from data import binance_data, bybit_data, coins_after_comparison, FEES
 
 
 # Function get funding from binance and bybit in set coins_after_comparison
-def get_better_funding(binance: dict, bybit: dict) -> dict:
+def get_funding(binance: dict, bybit: dict) -> dict:
     result = {}  # <class 'dict'>
+    for key in coins_after_comparison:
+        funding_A = (binance[key]['funding']) * 100  # Getting and calculation funding in percent from Binance
+        funding_B = (bybit[key]['funding']) * 100  # Getting and calculation funding in percent from Bybit
+        if not funding_A or not funding_B:
+            print(f'{key} ❌ нет данных')
+            continue
+        # Filter for getting necessary funding percent from exchanges
+        if (0.1 >= funding_A and -0.1 >= funding_A) or (0.1 >= funding_B and -0.1 >= funding_B):
+            result[key] = {
+                'binance': funding_A,
+                'bybit': funding_B,
+            }
+    return result
+
+
+funding = get_funding(binance_data, bybit_data)  # <class 'dict'>
+print(funding)
+for symbol, data in funding.items():
+    print(symbol, data['binance'], data['bybit'])
+
+
+def get_better_funding(binance: dict, bybit: dict) -> dict:
+    result = {}
     for key in coins_after_comparison:
         funding_A = (binance[key]['funding']) * 100  # Getting and calculation funding in percent from Binance
         funding_B = (bybit[key]['funding']) * 100  # Getting and calculation funding in percent from Bybit
@@ -19,23 +42,17 @@ def get_better_funding(binance: dict, bybit: dict) -> dict:
         if (0.1 >= funding_A and -0.1 >= funding_A) or (0.1 >= funding_B and -0.1 >= funding_B):
             if bigger_funding == 'Binance':  # Check bigger funding
                 result[key] = {
-                    'binance': funding_A,
-                    'bybit': funding_B,
                     'bigger funding': funding_A,
                 }
             elif bigger_funding == 'Bybit':  # Next can add same if for each exchange
                 result[key] = {
-                    'binance': funding_A,
-                    'bybit': funding_B,
                     'bigger funding': funding_B,
                 }
     return result
 
 
-funding = get_better_funding(binance_data, bybit_data)  # <class 'dict'>
-print(funding)
-for symbol, data in funding.items():
-    print(symbol, data['binance'], data['bybit'])
+better_funding = get_better_funding(binance_data, bybit_data)  # <class 'dict'>
+print(better_funding)
 
 
 def middle_price_exchanges(binance: dict, bybit: dict):
