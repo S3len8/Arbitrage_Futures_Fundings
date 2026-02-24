fe9f9ff03613b41a4eee15e585e177bee74ef252
Denys <glushychden.10@gmail.com>
2026-02-24 02:25:40 +0200

    Add database PostgreSQL.

---
+++ bd.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="Arbitrage_Futures_Funding",
+    user="postgres",
+    password="12345678",
+    port="5432"
+)
+
+cur = conn.cursor()
+
+cur.execute("SELECT version();")
+print(cur.fetchone())
+
+cur.close()
+conn.close()
