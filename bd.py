a1ce9c971854db4db1e6463c396978a7fe01913c
Denys <glushychden.10@gmail.com>
2026-02-24 19:47:15 +0200

    Trying new db and SQL-requests.

--- bd.py
+++ bd.py
@@ -7,8 +7,9 @@ conn = psycopg2.connect(
     password="12345678",
     port="5432"
 )
-
 cur = conn.cursor()
+cur.execute("SHOW data_directory;")
+print(cur.fetchone())
 
 cur.execute("SELECT version();")
 print(cur.fetchone())
